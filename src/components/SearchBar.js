74cb7687177a8e042c389a96f0332ecd0fdb6c85
Duy Tín <duytin095@gmail.com>
2024-04-07 18:31:05 +0700

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
+import {Feather} from '@expo/vector-icons';
 
 
 const SearchBar = ()=>{
     return <View style={styles.background}>
+        <Feather name="search" size={30}/>
         <Text>Search bar</Text>
     </View>
 }
